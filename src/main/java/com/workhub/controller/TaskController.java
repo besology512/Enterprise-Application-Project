c9ca9d0b39dc761ebd27f655136a2f4de7782a41
malakelbanna <malakelbanna2004@gmail.com>
2026-04-04 18:16:01 +0200

    fix: fix project creation and task patch bugs

--- src/main/java/com/workhub/controller/TaskController.java
+++ src/main/java/com/workhub/controller/TaskController.java
@@ -2,7 +2,6 @@ package com.workhub.controller;
 
 import com.workhub.model.Task;
 import com.workhub.service.TaskService;
-import jakarta.validation.Valid; // Added import
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +13,7 @@ public class TaskController {
     private final TaskService taskService;
 
     @PatchMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @Valid @RequestBody Task taskUpdates) {
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task taskUpdates) {
 
         Task updatedTask = taskService.updateTask(id, taskUpdates);
         return ResponseEntity.ok(updatedTask);
